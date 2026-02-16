67848907abfb3803b287df244d0f8cae43995265
TevaLautier <teva.lautier@gmail.com>
2026-02-16 18:56:46 +0100

    feat: version grid

--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>Hello from {{ name }}!</h1>
-    <a target="_blank" href="https://angular.dev/overview">
-      Learn more about Angular
-    </a>
-    <button (click)="counter.set(counter() - 1)">--</button>
-    <span> Counter: {{ counter() }} </span>
-    <button (click)="counter.set(counter() + 1)">++</button>
-  `,
+  templateUrl:'./main.html',
 })
 export class App {
   name = 'Angular';
